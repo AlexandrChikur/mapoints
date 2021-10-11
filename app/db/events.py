ed7a7a0b52c8686506e86ef08410d53f993c1c5a
Alexandr Chikur <alexandr.chikur@mail.ru>
2021-10-11 18:42:55 +0300

    Add db events

---
+++ app/db/events.py
@@ -0,0 +1,20 @@
+import asyncpg
+from fastapi import FastAPI
+from loguru import logger
+
+from app.core.config import settings
+
+
+async def connect_to_db(app: FastAPI) -> None:
+    logger.info(f"Connecting to the {settings.database_url}")
+    app.state.pool = await asyncpg.create_pool(
+        str(settings.database_url)
+    )
+    logger.info("Connection established successfully")
+    
+    
+async def disconnect_db(app: FastAPI) -> None:
+    logger.info(f"Connecting to the {settings.database_url} closing ...")
+    await app.state.conn.close()
+    
+    logger.info("Connection was closed successfully")
