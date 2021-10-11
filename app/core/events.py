96bed2be908b0aa8c4ffcb6d35ee1fd6dc7ea224
Alexandr Chikur <alexandr.chikur@mail.ru>
2021-10-11 19:04:30 +0300

    hot fix

---
+++ app/core/events.py
@@ -0,0 +1,22 @@
+from typing import Callable
+
+from fastapi import FastAPI
+from loguru import logger
+
+from app.db.events import connect_to_db, disconnect_db
+from app.db.repositories.points import PointsRepository
+
+
+def create_start_app_handler(app: FastAPI) -> Callable:
+    async def start_app() -> None:
+        await connect_to_db(app)
+        app.state.points_repo = PointsRepository(app.state.pool)
+    
+    return start_app
+
+
+def create_stop_app_handler(app: FastAPI) -> Callable:
+    async def stop_app() -> None:
+        await disconnect_db(app)
+
+    return stop_app
