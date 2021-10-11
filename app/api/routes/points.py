1edbf251e71c4613850836da581467df9d726c49
Alexandr Chikur <alexandr.chikur@mail.ru>
2021-10-11 21:32:10 +0300

    Change create point response & handle doesn't exist point error in get_point_by_uid() route

--- app/api/routes/points.py
+++ app/api/routes/points.py
@@ -1,10 +1,11 @@
-from typing import List, Optional
+from typing import Any, List, Optional, Union
 from uuid import UUID
 
-from fastapi import APIRouter, Body, Depends
+from fastapi import APIRouter, Body, Depends, HTTPException
 from starlette import status
 
 from app.api.dependencies.database import get_repository
+from app.db.errors import EtityDoesNotExistError
 from app.db.repositories.points import PointsRepository
 from app.models.schemas.points import Point, PointInDB, PointsInResponse
 
@@ -14,7 +15,7 @@ router = APIRouter()
 @router.post(
     "",
     status_code=status.HTTP_201_CREATED,
-    response_model=Point,
+    response_model=PointInDB,
     summary="Create Point",
 )
 async def create_point(
@@ -46,13 +47,16 @@ async def get_all_points(
 @router.get(
     "/{uid}",
     status_code=status.HTTP_200_OK,
-    response_model=PointInDB,
+    response_model=Union[PointInDB, Any],
     summary="Get point by UID",
 )
-async def get_all_points(
+async def get_point_by_uid(
     uid: UUID,
     points_repo: PointsRepository = Depends(get_repository(PointsRepository)),
-) -> PointInDB:
-    point = await points_repo.get_point_by_uid(uid=uid)
+) -> Union[PointInDB, Any]:
+    try:
+        point = await points_repo.get_point_by_uid(uid=uid)
+    except EtityDoesNotExistError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="not found")
 
     return point
