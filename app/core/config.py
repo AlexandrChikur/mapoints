006332b57624d4591cdda155ee18a78f8b3eb61f
Alexandr Chikur <alexandr.chikur@mail.ru>
2021-10-11 14:50:45 +0300

    fix version

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings, Field
 class Settings(BaseSettings):
     title: str = "MAPOINTS"
     description: str = "The API for building a map of the points and finding the best way."
-    version: str = "0.2.0"
+    version: str = "0.1.0"
     debug: bool = Field(False, dev="DEBUG")
     
     API_PREFIX: str = "/api"
