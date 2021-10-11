78cb27da0528d1090fd17aaa6e374de0bd3e572f
Alexandr Chikur <alexandr.chikur@mail.ru>
2021-10-11 16:08:47 +0300

    Change version to 0.1.1

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings, Field
 class Settings(BaseSettings):
     title: str = "MAPOINTS"
     description: str = "The API for building a map of the points and finding the best way."
-    version: str = "0.1.0"
+    version: str = "0.1.1"
     debug: bool = Field(False, env="DEBUG")
     
     API_PREFIX: str = "/api"
